80e88c49153d9f6162b874307c26605e3664acbc
kohsatoh1113 <r_baggio_no102006@yahoo.co.jp>
2015-11-07 16:09:50 +0900

    Add route root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   get 'medicine_masters/index'
-
   get 'hello/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'medicine_masters#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
