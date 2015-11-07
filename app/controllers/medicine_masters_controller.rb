e92de87f2a491e6607ce5d6bfbc397ffe27eadbb
kohsatoh1113 <r_baggio_no102006@yahoo.co.jp>
2015-11-07 15:54:56 +0900

    Fix bug.

--- app/controllers/medicine_masters_controller.rb
+++ app/controllers/medicine_masters_controller.rb
@@ -1,10 +1,10 @@
 class MedicineMastersController < ApplicationController
   def index
+    return @medicine_masters = [] unless params[:keyword]
     keywords = params[:keyword].tr('　',' ').split
     @medicine_masters = MedicineMaster
     keywords.each do |key|
       @medicine_masters = @medicine_masters.where('name like ?', "%#{key}%")
     end
-    @medicine_masters = [] unless params[:keyword]
   end
 end
